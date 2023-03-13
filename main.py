8e6a64a75d217504a5b3e79eea514c4733e9e608
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-13 13:08:28 -0400

    commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+url = "https://www.cbc.ca/news"
+
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+stories = soup.find_all('h3')
+
+data = []
+for story in stories:
+    title = story.text.strip()
+    if title not in data:
+        data.append(title)
+
+with open('webScrape/CBC.json', 'w') as f:
+    json.dump(data, f)
